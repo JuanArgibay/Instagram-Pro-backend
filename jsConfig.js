7719def51baa0c04ecbeeb5a37495327ad12ff4d
unknown <juan@DESKTOP-78O6N7O>
2023-09-19 21:39:26 +0200

    refactor to upload on website

--- jsConfig.js
+++ jsConfig.js
@@ -1,11 +1,13 @@
-const PORT = process.env.PORT  5002;
-const MYSQL_USER = process.env.MYSQL_USER  'root';
-const MYSQL_HOST = process.env.MYSQL_HOST  'localhost';
-const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD  '1234';
-const MYSQL_DB_NAME = process.env.MYSQL_DB_NAME  'p2p_db';
-const SMTP_USER = process.env.SMTP_USER  'player2playershop@gmail.com';
-const SMTP_PASSWORD = process.env.SMTP_PASSWORD  'xsmtpsib-f743ea96fa21218569f4f9106f466726e3dbc5c76a59129ef009174a300874e9-46gRYjJOxt7E39kH';
-const SECRET = process.env.SECRET  'maoihd093hdaknefifa3fadc';
+const PORT = process.env.PORT || 5002;
+const MYSQL_USER = process.env.MYSQL_USER || 'root';
+const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '1234';
+const MYSQL_DB_NAME = process.env.MYSQL_DB_NAME || 'p2p_db';
+const SENDGRID_API_KEY =
+    process.env.SENDGRID_API_KEY ||
+    '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';
+const SENDGRID_FROM = process.env.SENDGRID_FROM || 'minombre1986@gmail.com';
+const SECRET = process.env.SECRET || 'dc342425DWSFfwefw234';
 const UPLOADS_DIR = process.env.UPLOADS_DIR || 'uploads';
 
 module.exports = {
@@ -14,8 +16,7 @@ module.exports = {
     MYSQL_HOST,
     MYSQL_PASSWORD,
     MYSQL_DB_NAME,
-    SMTP_USER,
-    SMTP_PASSWORD,
     SECRET,
-    UPLOADS_DIR
+    UPLOADS_DIR,
+    SENDGRID_FROM,
 };
